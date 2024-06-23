e0d7a024a83a02056b44cad351ea80c2a4de3a88
hassaanistic <imhassaan.dev@gmail.com>
2024-06-23 12:08:50 +0500

    Additional User Routes Created

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -28,3 +28,61 @@ export const signin = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+export const getUserById = async (req, res) => {
+    const { userId } = req.params;
+    try {
+      const user = await User.findById(userId).select('-password');
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+  
+
+  export const updateUser = async (req, res) => {
+    const { userId } = req.params;
+    const { fullName, username, email, number, role, isVerified } = req.body;
+  
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        userId,
+        { fullName, username, email, number, role, isVerified },
+        { new: true } // Return the updated document
+      ).select('-password');
+  
+      if (!updatedUser) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+  
+      res.json(updatedUser);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+  
+  // Get all users
+  export const getAllUsers = async (req, res) => {
+    try {
+      const users = await User.find().select('-password');
+      res.json(users);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+  
+  // Delete user by ID
+  export const deleteUser = async (req, res) => {
+    const { userId } = req.params;
+    try {
+      const user = await User.findByIdAndDelete(userId);
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      res.json({ message: 'User deleted successfully' });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,18 @@
 import express from 'express';
 const router = express.Router();
 
-import { signup, signin } from '../controllers/user.controller.js';
+import { signup, signin, getAllUsers, getUserById, deleteUser , updateUser } from '../controllers/user.controller.js';
 
 // Routes for user signup and signin
 router.post('/signup', signup);
 router.post('/signin', signin);
+
+// Routes for getting all users and specific user by ID
+router.get('/', getAllUsers);
+router.get('/:userId', getUserById);
+
+// Route for deleting a user by ID
+router.put('/:userId', updateUser);
+router.delete('/:userId', deleteUser);
 
 export default router;
